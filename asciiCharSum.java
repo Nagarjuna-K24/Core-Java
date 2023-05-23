c2d4beccfe2a83710cf3e96df0c9557ec81ad2fd
Nagarjuna K <92105748+Nagarjuna-K24@users.noreply.github.com>
2023-05-23 19:22:28 +0530

    23-MAY-2023 :-- Core java practice changes
    Sum of int and short
    Sum of characters (ASCII value SUM of characters)

---
+++ asciiCharSum.java
@@ -0,0 +1,12 @@
+public class asciiCharSum {
+  public static void main (String args[]){
+    //short x = 999; 			// -32768 to 32767
+    //int   x = 99999; 		// -2147483648 to 2147483647
+    //long  x = 99999999999L; // -9223372036854775808 to 9223372036854775807
+    //short x = 999; 			// -32768 to 32767
+    //int   x = 99999; 		// -2147483648 to 2147483647
+    //long  x = 99999999999L; // -9223372036854775808 to 9223372036854775807
+
+    System.out.println('j' + 'a' + 'v' + 'a'); //418
+  }
+}
